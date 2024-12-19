2742a70c7e7f1df6293391a7d6f15324dbc1b9cc
Ectsang <1057844+Ectsang@users.noreply.github.com>
2024-12-19 05:35:45 +0000

    top nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { Web3Provider } from "@/components/Web3Provider";
 import { Toaster } from "@/components/ui/toaster";
 import { Navigation } from "@/components/Navigation";
-import { ConnectWallet } from "@/components/ConnectWallet";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,11 +16,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Web3Provider>
-          <div className="bg-violet-100 dark:bg-zinc-950 text-zinc-950 dark:text-neutral-100 ">
+          <div className="bg-background text-foreground">
             <Navigation />
-            <div className="fixed flex right-4 top-12">
-              <ConnectWallet />
-            </div>
             {children}
           </div>
         </Web3Provider>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,17 +6,16 @@ import { usePathname } from "next/navigation";
 export function Navigation() {
   const pathname = usePathname();
 
-  const getLinkClass = (path: string) =>
-    pathname === path
-      ? "text-zinc-700 font-bold"
-      : "text-violet-600 hover:text-violet-900 hover:font-bold";
+  // const getLinkClass = (path: string) =>
+  //   pathname === path
+  //     ? "text-zinc-700 font-bold"
+  //     : "text-violet-600 hover:text-violet-900 hover:font-bold";
+
   return (
-    <nav className="fixed">
-      <ul className="flex space-x-4">
-        <li className="m-4 ml-8">
-          <Link href="https://swap.stablecoin.xyz" target="_blank">
-            Swap
-          </Link>
+    <nav className="fixed w-full">
+      <ul className="flex justify-center space-x-4">
+        <li className="m-4">
+          <Link href="https://swap.stablecoin.xyz">Swap</Link>
         </li>
         <li className="m-4">
           <Link href="/" className="font-bold">
